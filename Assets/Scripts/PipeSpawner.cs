9dfc17fab5c8237ba72a3399c74f1c47ec8ee9b4
Koczka Balázs <koczka.balazs1201@gmail.com>
2023-11-22 14:07:15 +0100

    asd

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _maxTime = 1.5f;
+    [SerializeField] private float _heigthRange = .45f;
+    [SerializeField] private GameObject _pipe;
+    private float _timer;
 
-    // Update is called once per frame
+    void Start() => Spawn();
+
+
     void Update()
     {
-        
+        if (_timer > _maxTime)
+        {
+            Spawn();
+            _timer = 0;
+        }
+        _timer += Time.deltaTime;
+    }
+
+    private void Spawn()
+    {
+        Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-_heigthRange, +_heigthRange));
+        GameObject pipe = Instantiate(_pipe, spawnPos, Quaternion.identity);
+
+        Destroy(pipe, 10f);
     }
 }
